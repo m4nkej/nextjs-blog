d9506e1c4775459d961c294dcea3782ed3185d4f
m4nkej <mankej91@yahoo.pl>
2023-06-19 14:42:49 +0200

    Hero section implemented

---
+++ components/home-page/hero.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/image_home_page.png"
+          alt="Image showing mat"
+          width={400}
+          height={400}
+        />
+      </div>
+      <h1>HI im Mat</h1>
+      <p>Currently learning NextJS</p>
+    </section>
+  );
+}
+
+export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,13 @@
-function HomePage() {}
+import { Fragment } from "react";
+import Hero from "../components/home-page/hero";
+function HomePage() {
+  return (
+    <Fragment>
+      <Hero />
+      {
+        //<FeaturedPosts />
+      }
+    </Fragment>
+  );
+}
 export default HomePage;
